78e3b54a67b17e6c217abae6a084bbad53741e8c
MuhammedKAldin <54613628+MuhammedKAldin@users.noreply.github.com>
2022-09-22 17:20:00 +0200

    Create INI_Reader.cs

---
+++ INI_Reader.cs
@@ -0,0 +1,7 @@
+ReadINI("Offline",StaticDataManager.appSettings);
+
+public string ReadINI(string key, string path)
+{
+  var result = File.ReadAllText(path).Split('\r', '\n').First(st => st.StartsWith(key));
+  return result.Split('=')[1];
+}
